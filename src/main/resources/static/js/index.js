93b1c317838d02c72db9560303ac22fe822605fe
NayA000 <809104751@qq.com>
2018-10-07 21:20:19 +0800

    添加了钩钩框以及少量js函数

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,132 +1,185 @@
-
 var ulist = new Vue({
-	el: "#userlist",
-	data: {
-		uitems: [],
+	el : "#userlist",
+	data : {
+		uitems : [],
 	}
 })
 var blist = new Vue({
-	el: "#bgmlist",
-	data: {
-		bitems: [],
+	el : "#bgmlist",
+	data : {
+		bitems : [],
 	}
 })
 var tlist = new Vue({
-	el: "#topiclist",
-	data: {
-		titems: [],
+	el : "#topiclist",
+	data : {
+		titems : [],
 	}
 })
 
 var vlist = new Vue({
-	el: "#videolist",
-	data: {
-		vitems: [],
+	el : "#videolist",
+	data : {
+		vitems : [],
 	}
 })
 
 var balist = new Vue({
-	el: "#barragelist",
-	data: {
-		baitems: [],
+	el : "#barragelist",
+	data : {
+		baitems : [],
 	}
 })
-/*var divs = new Vue({
-	el: "#divs",
-	data: {
-		userShow : false,
-		bgmShow : true,
-		topicShow : true,
-		videoShow : true,
-		barragesShow : true
-	}
-})*/
-var url="http://192.168.1.5:8081";
-var serverUrl="http://192.168.1.7:8081";
+/*
+ * var divs = new Vue({ el: "#divs", data: { userShow : false, bgmShow : true,
+ * topicShow : true, videoShow : true, barragesShow : true } })
+ */
+//var url = "http://192.168.1.5:8081";
+var url = "http://localhost:8081";
+var serverUrl = "http://192.168.1.7:8081";
 var userPage = 0;
 var bgmPage = 0;
 var topicPage = 0;
 var videoPage = 0;
 var barragesPage = 0;
 
-function showDiv(flag){
-	var i=0;
-	var list=["#userdiv","#bgmdiv","#topicdiv","#videodiv","#barragediv"];
-	while(i!=5){
-		if(i!=flag){
-			$(list[i]).attr({ hidden:true })
-		}else{
-			$(list[i]).attr({ hidden:false })
+function showDiv(flag) {
+	var i = 0;
+	var list = [ "#userdiv", "#bgmdiv", "#topicdiv", "#videodiv", "#barragediv" ];
+	while (i != 5) {
+		if (i != flag) {
+			$(list[i]).attr({
+				hidden : true
+			})
+		} else {
+			$(list[i]).attr({
+				hidden : false
+			})
 		}
 		i++;
 	}
 }
 
-function getUserList(){
+function getUserList() {
 	console.log("点了一下");
 	$.ajax({
-		url : url+'/user/getAll?page='+userPage,
+		url : url + '/user/getAll?page=' + userPage,
 		type : 'GET',
-		//data: "page=0",
-		success: function(msg){
-		     console.log( msg);
-		     ulist.$data.uitems = msg.data;
-		   }
+		// data: "page=0",
+		success : function(msg) {
+			console.log(msg);
+			ulist.$data.uitems = msg.data;
+		}
 	});
 }
-function getBgmList(){
+function getBgmList() {
 	console.log("点了一下");
 	$.ajax({
-		url : url+'/bgm/getAll?page='+bgmPage,
+		url : url + '/bgm/getAll?page=' + bgmPage,
 		type : 'GET',
-		//data: "page=0",
-		success: function(msg){
-		     console.log( msg);
-		     blist.$data.bitems = msg.data;
-		   }
+		// data: "page=0",
+		success : function(msg) {
+			console.log(msg);
+			blist.$data.bitems = msg.data;
+		}
 	});
-	bgmPage+=1;
+	bgmPage += 1;
 }
-function getTopicList(){
+function getTopicList() {
 	console.log("点了一下");
 	$.ajax({
-		url : url+'/topic/getAll?page='+topicPage,
+		url : url + '/topic/getAll?page=' + topicPage,
 		type : 'GET',
-		//data: "page=0",
-		success: function(msg){
-		     console.log( msg);
-		     tlist.$data.titems = msg.data;
-		   }
+		// data: "page=0",
+		success : function(msg) {
+			console.log(msg);
+			tlist.$data.titems = msg.data;
+		}
 	});
-	topicPage+=1;
+	topicPage += 1;
 }
-function getVideoList(){
+function getVideoList() {
 	console.log("点了一下");
 	$.ajax({
-		url : url+'/video/getAll?page='+videoPage,
+		url : url + '/video/getAll?page=' + videoPage,
 		type : 'GET',
-		//data: "page=0",
-		success: function(msg){
-		     console.log( msg);
-		     vlist.$data.vitems = msg.data;
-		   }
+		// data: "page=0",
+		success : function(msg) {
+			console.log(msg);
+			vlist.$data.vitems = msg.data;
+		}
 	});
-	videoPage+=1;
+	videoPage += 1;
 }
 
-function getBarrageList(){
+function getBarrageList() {
 	console.log("点了一下");
 	$.ajax({
-		url : url+'/barrage/getAll?page='+barragesPage,
+		url : url + '/barrage/getAll?page=' + barragesPage,
 		type : 'GET',
-		//data: "page=0",
-		success: function(msg){
-		     console.log( msg);
-		     balist.$data.baitems = msg.data;
-		   }
+		// data: "page=0",
+		success : function(msg) {
+			console.log(msg);
+			balist.$data.baitems = msg.data;
+		}
 	});
-	barragesPage+=1;
+	barragesPage += 1;
 }
+// ------------------------------------------------------------
+// day 2
+var userCheck = [];
+var bgmCheck = [];
+var topicCheck = [];
+var videoCheck = [];
+var barrageCheck = [];
+var check=[userCheck,bgmCheck,topicCheck,videoCheck,barrageCheck]
+function getCheck(flag,name) {
+	obj = document.getElementsByName(name);
+	check[flag] = [];
+	for (k in obj) {
+		if (obj[k].checked)
+			check[flag].push(obj[k].value);
+	}
+	alert(check[flag]);
+}
+//------------------------------------------
+//topic-->delect     search
+function topicDelect(){
+	getCheck(2,'topicCheck');
+	$.ajax({
+		url : url + '/topic/delect' ,
+		type : 'GET',
+		data: {
+			tid: check[2],
+		}
+		success : function(msg) {
+			console.log(msg);
+			if(topicPage>0)
+				topicPage-=1;
+			getTopicList();
+		}
+	});
+}
+var topicSearchPage = 0;
+function topicSearch(){
+	var key = $("#topicKey").attr("value");
+	$.ajax({
+		url : url + '/topic/search' ,
+		type : 'GET',
+		data: {
+			key: key,
+			page: topicSearchPage
+		}
+		success : function(msg) {
+			console.log(msg);
+			tlist.$data.titems = msg.data;
+		}
+	});
+}
+
+
+
+
 
 
 
