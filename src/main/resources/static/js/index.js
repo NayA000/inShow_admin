5c6c8e3a861a00361f61ae276226f9220ab8144c
AirYuBaby <1152519350@qq.com>
2018-10-12 10:46:19 +0800

    更新主页

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -30,22 +30,55 @@ var balist = new Vue({
 		baitems : [],
 	}
 })
+
+
+
+$(document).ready(function(){
+	$("#userdiv").attr({
+		hidden : false
+	})
+	getUserList();
+  })
 /*
  * var divs = new Vue({ el: "#divs", data: { userShow : false, bgmShow : true,
  * topicShow : true, videoShow : true, barragesShow : true } })
  */
 //var url = "http://192.168.1.5:8081";
-var url = "http://localhost:8081";
+var url = "http://localhost:8080";
 var serverUrl = "http://192.168.1.7:8081";
-var userPage = 0;
-var bgmPage = 0;
-var topicPage = 0;
-var videoPage = 0;
-var barragePage = 0;
+var userPage = 1;
+var bgmPage = 1;
+var topicPage = 1;
+var videoPage = 1;
+var barragePage = 1;
 
 function showDiv(flag) {
 	var i = 0;
 	var list = [ "#userdiv", "#bgmdiv", "#topicdiv", "#videodiv", "#barragediv" ];
+	switch(flag){
+	case 0:
+		userPage = 1;
+		getUserList();
+		break;
+	case 1:
+		bgmPage = 1;
+		getBgmList();
+		break;
+	case 2:
+		topicPage = 1;
+		getTopicList();
+		break;
+	case 3:
+		videoPage = 1;
+		getVideoList();
+		break;
+	case 4:
+		barragePage = 1;
+		getBarrageList();
+		break;
+	default:
+		break;
+	}
 	while (i != 5) {
 		if (i != flag) {
 			$(list[i]).attr({
@@ -126,6 +159,95 @@ function getBarrageList() {
 	});
 	barragePage += 1;
 }
+//---------------------------------------------------------------
+//上一页
+function getUserList_1() {
+	if(userPage>=3) {
+		userPage-=2;
+		console.log("点了一下");
+		$.ajax({
+			url : url + '/user/getAll?page=' + userPage,
+			type : 'GET',
+			// data: "page=0",
+			success : function(msg) {
+				console.log(msg);
+				ulist.$data.uitems = msg.data;
+			}
+		});
+		userPage+=1;
+	}
+	
+}
+function getBgmList_1() {
+	if(bgmPage>=3) {
+		bgmPage-=2;
+		console.log("点了一下");
+		$.ajax({
+			url : url + '/bgm/getAll?page=' + bgmPage,
+			type : 'GET',
+			// data: "page=0",
+			success : function(msg) {
+				console.log(msg);
+				blist.$data.bitems = msg.data;
+			}
+		});
+		bgmPage += 1;
+	}
+	
+}
+function getTopicList_1() {
+	if(topicPage>=3) {
+		topicPage-=2;
+		console.log("点了一下");
+		$.ajax({
+			url : url + '/topic/getAll?page=' + topicPage,
+			type : 'GET',
+			// data: "page=0",
+			success : function(msg) {
+				console.log(msg);
+				tlist.$data.titems = msg.data;
+			}
+		});
+		topicPage += 1;
+	}
+	
+}
+function getVideoList_1() {
+	if(videoPage>=3) {
+		videoPage-=2;
+		console.log("点了一下");
+		$.ajax({
+			url : url + '/video/getAll?page=' + videoPage,
+			type : 'GET',
+			// data: "page=0",
+			success : function(msg) {
+				console.log(msg);
+				vlist.$data.vitems = msg.data;
+			}
+		});
+		videoPage += 1;
+	}
+	
+}
+
+function getBarrageList_1() {
+	if(barragePage>=3) {
+		barragePage-=2;
+		console.log("点了一下");
+		$.ajax({
+			url : url + '/barrage/getAll?page=' + barragePage,
+			type : 'GET',
+			// data: "page=0",
+			success : function(msg) {
+				console.log(msg);
+				balist.$data.baitems = msg.data;
+			}
+		});
+		barragePage += 1;
+	}
+	
+}
+
 // ------------------------------------------------------------
 // day 2
 var userCheck = [];
@@ -299,12 +421,4 @@ function barrageDelect(){
 		}
 	});
 }
-
-
-
-
-
-
-
-
 
